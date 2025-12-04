2e312874aed888123cbe998a86e748781011d4fe
Бексултан <nightwing_14@mail.ru>
2025-12-04 17:49:42 +0300

    HW

---
+++ src/main/java/org/skypro/skyshop/model/service/BasketService.java
@@ -0,0 +1,44 @@
+package org.skypro.skyshop.model.service;
+
+import org.skypro.skyshop.model.BasketItem;
+import org.skypro.skyshop.model.UserBasket;
+import org.skypro.skyshop.model.basket.ProductBasket;
+import org.skypro.skyshop.model.product.Product;
+
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+public class BasketService {
+    private final ProductBasket productBasket;
+    private final StorageService storageService;
+
+    public BasketService(ProductBasket productBasket, StorageService storageService) {
+        this.productBasket = productBasket;
+        this.storageService = storageService;
+    }
+
+    public void addProductById(UUID uuid) {
+
+        if (storageService.getProductById(uuid).isPresent() == false) {
+            throw new IllegalArgumentException()
+            ;
+        }
+        productBasket.addProduct(uuid);
+    }
+
+    public UserBasket getUserBasket() {
+        Map<UUID, Integer> productsInBasket = productBasket.getAllProducts();
+        List<BasketItem> basketItems = productsInBasket.entrySet().stream()
+                .map(entry -> {
+                    UUID productId = entry.getKey();
+                    int count = entry.getValue();
+                    Product product = storageService.getProductById(productId)
+                            .orElseThrow(() -> new IllegalStateException());
+                    return new BasketItem(product, count);
+                })
+                .collect(Collectors.toList());
+        return new UserBasket(basketItems);
+    }
+}
